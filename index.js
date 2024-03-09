b288f3cae2dc1481b4bee41812356d7e2769d3db
Andres G <agh92@hotmail.com>
2024-03-09 13:48:20 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const fetchPuzzleData = () => fetch("https://devrel.wearedevelopers.com/code100-puzzles/017-stars/stars.json")
-const elipsisArea = (width, height) => Math.PI * width/2 * height/2
-const squareArea = (width, height) => width * height
+const sum = (a, b) => a + b;
+const elipsisArea = (majorLength, minorLength) => Math.PI * majorLength/2 * minorLength/2
+const rectangleArea = (width, height) => width * height
+const calculateGreenPixels = image => rectangleArea(image.width, image.height) - elipsisArea(image.width, image.height);
+
+const sumAllGreenPixels = images => {
+    const sumOfgreenPixels = images
+        .map(calculateGreenPixels)
+        .reduce(sum)
+
+    return Math.floor(sumOfgreenPixels)
+};
 
 const solvePuzzle = async () => {
     const puzzleData = await fetchPuzzleData()
     const images = await puzzleData.json()
-    const greenPixels = images
-        .map(image => squareArea(image.width, image.height) - elipsisArea(image.width, image.height))
-        .reduce((a, b) => a + b)
-
-    console.log(Math.floor(greenPixels))
+    return sumAllGreenPixels(images)
 }
 
-solvePuzzle()
+solvePuzzle().then(console.log)
